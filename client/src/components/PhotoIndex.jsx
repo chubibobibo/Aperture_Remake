85bbac9db93724aa40761bfef6b51ff5d12f2e79
Alistair Abao <abaolester@gmail.com>
2024-10-20 13:00:59 +0200

    finalizing for deploy

--- client/src/components/PhotoIndex.jsx
+++ client/src/components/PhotoIndex.jsx
@@ -1,13 +1,18 @@
 // import { useContext } from "react";
-import { useNavigate, redirect, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // import PhotosContext from "../context/PhotoContext";
+
+// import { lazy, Suspense } from "react";
+// import Loading from "./components/Loading.jsx";
+
+// const PhotoIndex = lazy(() => import("../../components/PhotoIndex.jsx"));
 
 import UserAvatar from "./UserAvatar";
 import { usePhotoContext } from "../hooks/usePhotoContext";
 import { Typography } from "@material-tailwind/react";
-import { toCapitalize } from "../utils/toCaptialize";
 
-import PropTypes from "prop-types";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function PhotoIndex() {
   /** Obtain data from contexts */
@@ -25,67 +30,36 @@ function PhotoIndex() {
   };
 
   return (
-    <masonry-layout gap={100}>
-      <section className='md:w-screen md:flex md:justify-center'>
-        {photos.length === 0 ? (
-          <Typography variant='h6' className='mt-12 md:text-2xl'>
-            Soooo quiet in here. Create a post
-          </Typography>
-        ) : (
-          <div className='grid grid-cols-2 gap-3 mt-1 md:grid-cols-3 w-full md:w-11/12 md:mt-10'>
-            {photos.map((newData) => {
-              // console.log(newData);
-              return (
-                <div key={newData._id} className='flex mt-4 flex-col gap-1'>
-                  <UserAvatar newData={newData} />
-                  <img
-                    className='object-cover h-[10rem] w-[10rem] sm:h-[15rem] sm:w-[20rem] xl:h-[27rem] xl:w-full'
-                    src={newData.photoUrl}
-                    alt='gallery-photo'
-                    loading='lazy'
-                    onClick={() => {
-                      handleClickNav(newData._id);
-                    }}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        )}
-      </section>
-    </masonry-layout>
+    <section className='md:w-screen md:flex md:justify-center'>
+      {photos.length === 0 ? (
+        <Typography variant='h6' className='mt-12 md:text-2xl'>
+          Soooo quiet in here. Create a post
+        </Typography>
+      ) : (
+        <div className='grid grid-cols-2 gap-3 mt-1 md:grid-cols-3 w-full md:w-11/12 md:mt-10'>
+          {photos.map((newData) => {
+            // console.log(newData);
+            return (
+              <div key={newData._id} className='flex mt-4 flex-col gap-1'>
+                <UserAvatar newData={newData} />
+                <LazyLoadImage
+                  effect='blur'
+                  className='object-cover h-[10rem] w-[10rem] sm:h-[15rem] sm:w-[20rem] xl:h-[27rem] xl:w-full cursor-pointer'
+                  src={newData.photoUrl}
+                  alt='gallery-photo'
+                  loading='lazy'
+                  onClick={() => {
+                    handleClickNav(newData._id);
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </section>
+
+    // <PhotoComponent photos={photos} handleClickNav={handleClickNav} />
   );
 }
 export default PhotoIndex;
-
-{
-  /* <section className='md:w-screen md:flex md:justify-center'>
-  {photos.length === 0 ? (
-    <Typography variant='h6' className='mt-12 md:text-2xl'>
-      Soooo quiet in here. Create a post
-    </Typography>
-  ) : (
-    <div className='grid grid-cols-2 gap-3 mt-1 md:grid-cols-3 md:w-11/12 md:mt-10'>
-      {photos.map((newData) => {
-        // console.log(newData);
-        return (
-          <div key={newData._id} className='flex mt-4 flex-col gap-1'>
-            <UserAvatar newData={newData} />
-            <img
-              className='h-auto max-w-full rounded-lg object-cover object-center cursor-pointer'
-              src={newData.photoUrl}
-              alt='gallery-photo'
-              loading='lazy'
-              onClick={() => {
-                handleClickNav(newData._id);
-              }}
-            />
-          </div>
-        );
-      })}
-    </div>
-  )}
-</section>; */
-}
-
-// h-auto max-w-full rounded-lg object-center cursor-pointer mb-2 max-h-[6rem] object-cover xs:max-h-[9rem] sm:max-h-[10rem] md:max-w-full md:max-h-[9rem] xl:max-h-[13rem]
