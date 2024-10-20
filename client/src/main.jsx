7ef0628cd4e4407217ae8b874530dcbd32f90d05
Alistair Abao <abaolester@gmail.com>
2024-10-20 16:48:37 +0200

    deploy ready

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -11,10 +11,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ToastContainer position='top-center' />
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ToastContainer position='top-center' />
+  </QueryClientProvider>
 );
