b8753681118594a68f557cc9265d58438f9ff13d
Alistair Abao <abaolester@gmail.com>
2024-10-27 14:51:57 +0100

    fix for the limiter api

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -28,7 +28,7 @@ const limiter = rateLimit({
   limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: "Too many login attempts. Try again in 15 minutes ",
+  message: "Too many login attempts. Try again in 15 minutes",
 });
 
 /** GET LOGGED USER */
